38b162f4e794e4af61a1671cdbc67182ecde6423
kieranhoggmv <kieran.hogg@multiverse.io>
2026-09-23 10:20:52 +0100

    Create setup.sh

---
+++ module5/setup.sh
@@ -0,0 +1,53 @@
+#!/usr/bin/env bash
+
+# Install dependencies
+sudo apt update
+sudo apt install ca-certificates curl -y
+
+# Install docker
+sudo install -m 0755 -d /etc/apt/keyrings
+sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
+sudo chmod a+r /etc/apt/keyrings/docker.asc
+sudo tee /etc/apt/sources.list.d/docker.sources <<EOF
+Types: deb
+URIs: https://download.docker.com/linux/ubuntu
+Suites: $(. /etc/os-release && echo "${UBUNTU_CODENAME:-$VERSION_CODENAME}")
+Components: stable
+Architectures: $(dpkg --print-architecture)
+Signed-By: /etc/apt/keyrings/docker.asc
+EOF
+sudo apt update
+VERSION_STRING=5:29.6.1-1~ubuntu.24.04~noble
+sudo apt install docker-ce=$VERSION_STRING docker-ce-cli=$VERSION_STRING containerd.io docker-buildx-plugin docker-compose-plugin -y
+sudo groupadd docker
+sudo usermod -aG docker $USER
+
+# Install github-cli
+(type -p wget >/dev/null || (sudo apt update && sudo apt install wget -y)) \
+  && sudo mkdir -p -m 755 /etc/apt/keyrings \
+  && out=$(mktemp) && wget -nv -O$out https://cli.github.com/packages/githubcli-archive-keyring.gpg \
+  && cat $out | sudo tee /etc/apt/keyrings/githubcli-archive-keyring.gpg > /dev/null \
+  && sudo chmod go+r /etc/apt/keyrings/githubcli-archive-keyring.gpg \
+  && sudo mkdir -p -m 755 /etc/apt/sources.list.d \
+  && echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/githubcli-archive-keyring.gpg] https://cli.github.com/packages stable main" | sudo tee /etc/apt/sources.list.d/github-cli.list > /dev/null \
+  && sudo apt update \
+  && sudo apt install gh -y
+
+# Install act
+gh extension install https://github.com/nektos/gh-act --pin v0.2.89
+echo 'alias act="gh act --action-offline-mode"' >> ~/.bashrc
+
+# Configure act
+mkdir -p ~/.config/act
+tee ~/.config/act/actrc <<EOF
+-P ubuntu-latest=catthehacker/ubuntu:act-latest
+-s GITHUB_TOKEN=${GH_TOKEN}
+EOF
+
+# Verify versions
+gh --version
+gh act --version
+docker --version
+
+# Refresh the user login
+exec sudo -iu $USER
